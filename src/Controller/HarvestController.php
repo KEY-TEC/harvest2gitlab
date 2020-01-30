921cdc8a15e83b0026b3516ac20778f170751773
Valentin Meyer <valentin.meyer@key-tec.de>
2020-01-30 17:49:13 +0100

    harvest2gitlab

--- src/Controller/HarvestController.php
+++ src/Controller/HarvestController.php
@@ -7,6 +7,7 @@ use App\Service\ImportServiceInterface;
 use Http\Client\Common\Exception\ClientErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class GitlabController.
